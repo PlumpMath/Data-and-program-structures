5c9c7887aa5dcbdcb0f7a78bb951a8266d340c8b
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-04-13 09:32:53 +0000

    Lab1  - Corrected repeated application so to apply only the specified number of times.

--- komplettering/Lab1/lab1.py
+++ komplettering/Lab1/lab1.py
@@ -1,12 +1,12 @@
-#shortcut functions
+# Shortcut functions
 def cube(x): return x*x*x
 def square(x): return x*x
-def times(x,y): return x*y
-def add(x,y): return x+y
+def times(x, y): return x*y
+def add(x, y): return x+y
 def next(x): return x+1
 
-# Lab 1
 
+# Lab 1
 
 def sum(term, lower, successor, upper):
 	if lower > upper:
@@ -22,7 +22,7 @@ def sum_iter(term, lower, successor, upper):
 			return result
 		else:
 			return iter(successor(lower), result + term(lower))
-	return iter(lower,0)
+	return iter(lower, 0)
 # every step of the recursion calculates it's own result, no need to save everything on the stack.
 
 ###################################################################
@@ -41,87 +41,89 @@ def product_iter(term, lower, successor, upper):
 			return result
 		else:
 			return iter_p(successor(lower), result * term(lower))
-	return iter_p(lower,1)
+	return iter_p(lower, 1)
 
 ###################################################################
 
 def factorial(value):
-    return product((lambda n: n),2,(lambda n: n + 1),value)
+    return product((lambda n: n), 2, (lambda n: n + 1), value)
 
 def even_function(n):
 	if (n % 2 == 0):
 		return (n+2)/(n+1)
 	else:
 		return (n+1)/(n+2)
-	
+
 def approx_pi (n):
-	return 4*product_iter(even_function, 1, next, n) 
+	return 4*product_iter(even_function, 1, next, n)
 
 ###################################################################
 
-def accumulate (combiner, null, term, lower, succ, upper):
+def accumulate(combiner, null, term, lower, succ, upper):
 	if lower > upper:
 		return null
 	else:
 		return combiner(term(lower),  accumulate(combiner, null, term, succ(lower), succ, upper))
 
 
-def accumulate_iter (combiner, null, term, lower, succ, upper):
+def accumulate_iter(combiner, null, term, lower, succ, upper):
 	def iter_a(lower, result):
-		if lower > upper :
+		if succ(lower) >= upper:
 			return result
 		else:
 			return iter_a(succ(lower), combiner(result,  term(lower)))
-	return iter_a(lower,null)
+	return iter_a(lower, null)
 
-#c) non-linear functions ?	
-	
+#c) non-linear functions ?
+
 ###################################################################
 
-def foldl (f,n,lista):
-    if len(lista) == 0:
+def foldl (f, n, lista):
+    if lista:
         return n
     else :
-        return foldl(f,f(n,lista[0]),lista[1:])
+        return foldl(f, f(n, lista[0]), lista[1:])
 
-def foldr (f,n,lista):
-    if len(lista) == 0:
+def foldr (f, n, lista):
+    if lista:
         return n
     else :
-        return foldr(f,f(n,lista[-1]),lista[:-1])
+        return foldr(f, f(n, lista[-1]), lista[:-1])
 
 ###################################################################
 
 def my_map(f, seq):
-    return (foldl((lambda y,x: y+[f(x)]), [], seq))
+    return (foldl((lambda y, x: y+[f(x)]), [], seq))
 
 def reverse_r (seq):
-    return foldr((lambda x,y : x+[y]), [], seq)
+    return foldr((lambda x, y : x+[y]), [], seq)
 
 def reverse_l (seq):
-    return foldl((lambda x,y : [y]+x), [], seq)
+    return foldl((lambda x, y : [y]+x), [], seq)
 
 ###################################################################
 
 
-def repeat(f,n):
+def repeat(f, n):
 	if n == 0:
 		return (lambda x: x)
 	else:
-		return (lambda x: f(repeat(f,n-1)(x)))
+		return (lambda x: f(repeat(f, n-1)(x)))
 
 # f,g => f o g = f(g(x))
-def compose(f,g):
+def compose(f, g):
 	return (lambda x: f(g(x)))
 
-	
-	
-def repeated_application(f,n):
-	return accumulate_iter (compose, f, (lambda x: f), 0, next, n)
-	
+
+
+def repeated_application(f, n):
+	if n == 0:
+		return (lambda x: x)
+	return accumulate_iter(compose, f, (lambda x: f), 0, next, n)
+
 # iter_a(succ(lower), combiner(result,  term(lower)))
 # target for accumulate: compose (old_f , new_f )
-# term must remove pendence on lower? -> (lambda x: f)	
+# term must remove pendence on lower? -> (lambda x: f)
 # start with f
 # combiner is compose
 # go from 0 to N (number of iterations)
@@ -132,18 +134,18 @@ def repeated_application(f,n):
 def smooth(f):
 	return (lambda x: ( f(x + 0.01) + f(x) + f(x - 0.01) ) / 3 )
 
-def n_fold_smooth(f,n):
+def n_fold_smooth(f, n):
 	if n == 0:
 		return f
 	else:
-		return(lambda x: (repeat(smooth,n))(f)(x))
+		return(lambda x: (repeat(smooth, n))(f)(x))
 # repeat f,4 -> f(f(f(f(x))))
 # smooth f,4 -> (f(4.01) + f(4) + f(3.99))/3 -> y
 # om repeat tar in smooth -> smooth(smooth(x)) dvs x = f?
 # --> argument i 2 steg? smooth(smooth(x)) -> smooth(smooth(f(x))) = y(x)
 
 
-		
+
 ###################################################################
 ###################################################################
 ###################################################################
@@ -154,58 +156,60 @@ def test():
 def output_function():
     x = 5
     y = 10
-    seq = [1,2,3,4,5]
+    seq = [1, 2, 3, 4, 5]
 
     print("----------- Sum ---------")
     print(" recursive ")
-    print("result: {0}".format(sum((lambda n: n),x,next,y)))
+    print("result: {0}".format(sum((lambda n: n), x, next, y)))
     print(" tail recursive ")
-    print("result: {0}".format(sum_iter((lambda n: n),x,next,y)))
+    print("result: {0}".format(sum_iter((lambda n: n), x, next, y)))
 
     print("----------- Product ---------")
     print(" recursive ")
-    print("result: {0}".format(product((lambda n: n),x,next,y)))
+    print("result: {0}".format(product((lambda n: n), x, next, y)))
     print(" tail recursive ")
-    print("result: {0}".format(product_iter((lambda n: n),x,next,y)))
+    print("result: {0}".format(product_iter((lambda n: n), x, next, y)))
 
     print("----------- factorial ---------")
     print(" based on recursive product ")
     print("result: {0}".format(factorial(x)))
     print("Pi approximation: {0}".format(approx_pi(800)))
-    
+
     print("----------- Accumulators ---------")
-    print("accumulate sum result: {0}".format(accumulate(add,0,(lambda n: n),x,next,y)))
-    print("accumulate product result: {0}".format(accumulate(times,1,(lambda n: n),x,next,y)))
-    print("accumulate sum_iter result: {0}".format(accumulate_iter(add,0,(lambda n: n),x,next,y)))
-    print("accumulate product_iter result: {0}".format(accumulate_iter(times,1,(lambda n: n),x,next,y)))
+    print("accumulate sum result: {0}".format(accumulate(add, 0, (lambda n: n), x, next, y)))
+    print("accumulate product result: {0}".format(accumulate(times, 1, (lambda n: n), x, next, y)))
+    print("accumulate sum_iter result: {0}".format(accumulate_iter(add, 0, (lambda n: n), x, next, y)))
+    print("accumulate product_iter result: {0}".format(accumulate_iter(times, 1, (lambda n: n), x, next, y)))
 
 
     print("----------- Folds left and right ---------")
-    print(" seq = ",seq)
-    print(" foldl(times,2,seq): ", foldl(times,2,seq))
-    print(" foldr(add,0,seq): ", foldr(add,0,seq))
-    print(" my_map(cube,seq) = ",my_map(cube,seq))
-    print(" reverse_l(seq) = ",reverse_l(seq))
-    print(" reverse_r(seq) = ",reverse_r(seq))
+    print(" seq = ", seq)
+    print(" foldl(times, 2, seq): ", foldl(times, 2, seq))
+    print(" foldr(add, 0, seq): ", foldr(add, 0, seq))
+    print(" my_map(cube, seq) = ", my_map(cube, seq))
+    print(" reverse_l(seq) = ", reverse_l(seq))
+    print(" reverse_r(seq) = ", reverse_r(seq))
 
     print("----------- Repeat -----------")
-    sq_twice = repeat(square,2)    
-    print("repeat square twice with input 5 result: ", sq_twice(5))    
+    sq_twice = repeat(square, 2)
+    print("repeat square twice with input 5 result: ", sq_twice(5))
     print("Smooth square of 4 result: ", smooth(square)(4))
-    five_sm = n_fold_smooth(square,5)
-    print("5-fold-smooth square of 4 result: ",five_sm(4))
-    regular = n_fold_smooth(square,0)
-    print("regular square of 4 result: ",regular(4))
+    five_sm = n_fold_smooth(square, 5)
+    print("5-fold-smooth square of 4 result: ", five_sm(4))
+    regular = n_fold_smooth(square, 0)
+    print("regular square of 4 result: ", regular(4))
 
-
+    print("Single 'repeated' application of 5^2: ", repeated_application(square, 1)(5))
+    print("Not even once 'repeated' application of 5^2: ", repeated_application(square, 0)(5))
+    print("Repeat 5^2 zero times: ", repeat(square, 0)(5))
     print(test())
 output_function()
 
 
 
 #2.1
-#python seems to try and validate the arguments before executing the function. 
-#Both f and test are considered ok as functions, but when you use f as an argument for test it seems like we get a infinite f()-call. 
+#python seems to try and validate the arguments before executing the function.
+#Both f and test are considered ok as functions, but when you use f as an argument for test it seems like we get a infinite f()-call.
 
 #2.2
 #print_mess is declared as a function-call with a preset-argument?
@@ -234,8 +238,5 @@ output_function()
 #f
 
 #c)
-#dynamic scoping would lead to f inheriting values/arguments from g. 
+#dynamic scoping would lead to f inheriting values/arguments from g.
 #x in g's local scope would be assessible from f.
-
-
-
