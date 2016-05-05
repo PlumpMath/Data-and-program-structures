def302fe005c8dc5e532265371d0600f5da42d99
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-05-05 12:46:10 +0000

    Lab 4 - Initial non-working implementation of BoundCheckingDecorator.

--- Lab4/BoundCheckingDecorator.py
+++ Lab4/BoundCheckingDecorator.py
@@ -1,10 +1,25 @@
+from functools import wraps
+
 # Change bound_checking_decorator to handle function with multiple arguments
 
-def bound_checking_decorator(min, max):
-  def make_decorator(func):
-    def decorator(x):
-      if(x < min or x > max):
-        raise Exception()
-      return func(x)
-    return decorator
-  return make_decorator
+# def bound_checking_decorator(min, max):
+#   def make_decorator(func):
+#     def decorator(x):
+#       if(x < min or x > max):
+#         raise Exception()
+#       return func(x)
+#     return decorator
+#   return make_decorator
+
+def bound_checking_decorator(*dec_args):
+  if len(dec_args) % 2 != 0:
+    raise Exception("Uneven number of detorator arguments given.")
+  def make_detorator(func):
+    @wraps(func)
+    def detorator(*args):
+      for i in range(len(args)):
+        if args[i] < dec_args[i * 2] or args[i] > dec_args[i * 2 + 1]:
+          raise Exception("Out of bounds.")
+        return func(*args)
+      return decorator
+    return make_decorator
