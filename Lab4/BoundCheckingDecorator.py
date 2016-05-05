8a9b61676240f52700693de5c8518df51c008093
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-05-05 13:33:51 +0000

    A more Pythonic version of BoundCheckingDecorator.py

--- Lab4/BoundCheckingDecorator.py
+++ Lab4/BoundCheckingDecorator.py
@@ -1,25 +1,17 @@
 from functools import wraps
 
-# Change bound_checking_decorator to handle function with multiple arguments
-
-# def bound_checking_decorator(min, max):
-#   def make_decorator(func):
-#     def decorator(x):
-#       if(x < min or x > max):
-#         raise Exception()
-#       return func(x)
-#     return decorator
-#   return make_decorator
-
-def bound_checking_decorator(*dec_args):
-  if len(dec_args) % 2 != 0:
-    raise Exception("Uneven number of detorator arguments given.")
-  def make_decorator(func):
-    @wraps(func)
-    def decorator(*args):
-      for i in range(len(args)):
-        if args[i] < dec_args[i * 2] or args[i] > dec_args[i * 2 + 1]:
-          raise Exception("Out of bounds.")
-      return func(*args)
-    return decorator
-  return make_decorator
+# Change bound_checking_decorator to handle function with multiple arguments	
+def bound_checking_decorator(*bounds)
+	if len(bounds) % 2 != 0:
+		raise Exception("Uneven number of detorator arguments given.")	
+	def make_decorator(func):
+		@wraps(func)
+		def decorator(*args)
+			if not (len(bounds)/2 == len(args))
+				raise Exception("Number of bounds doesn't match the number of arguments.")
+			for min,max,value in zip(bounds[0::2], bounds[1::2], args):
+				if not (min <= value and max >= value)
+					raise Exception("Out of bounds.")
+			return func(*args)
+		return decorator
+	return make_decorator
