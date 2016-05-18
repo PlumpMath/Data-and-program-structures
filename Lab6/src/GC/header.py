2167d6093be5644c49c3f044a015b936be80f215
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-05-18 11:24:00 +0000

    Lab 6 - Passing get_header_garbage test.

--- Lab6/src/GC/header.py
+++ Lab6/src/GC/header.py
@@ -1,26 +1,37 @@
 def header_get_garbage_flag(heap, pointer):
-  pass
+  mask = 0b01000000
+  return heap[pointer] & mask == 0
+
 
 def header_set_garbage_flag(heap, pointer, value):
   pass
 
 def header_get_used_flag(heap, pointer):
   pass
-  
+
 def header_set_used_flag(heap, pointer, value):
   pass
 
 def header_is_pointers_array(heap, pointer):
   pass
-  
+
 def header_mark_as_pointers_array(heap, pointer):
   pass
 
 def header_mark_as_bytes_array(heap, pointer):
   pass
-  
+
 def header_get_size(heap, pointer):
-  pass
+  mask_size = 0b00001111
+  print("Get size:", bin(mask_size))
+  number = bytearray()
+  number.append(heap[pointer] & mask_size)
+  print("Heap:", heap[pointer])
+  print("Masked heap:", heap[pointer] & mask_size)
+  for i in range (1, 4):
+    number.append(heap[pointer+i])
+  print(number)
+  return int.from_bytes(number, 'big')
 
 def header_set_size(heap, pointer, size):
   pass
--- Lab6/src/GC/heap.py
+++ Lab6/src/GC/heap.py
@@ -1,24 +1,24 @@
-from .header import * 
+from .header import *
 from .pointers_array import *
 
 class heap(object):
   # size: the size (in bytes) of the heap
   def __init__(self, size):
-    self.data             = bytearray(size)
+    self.data = bytearray(size)
     pass
-  
+
   # return the index to the begining of a block with size (in bytes)
   def allocate(self, size):
     pass
-  
+
   # unallocate the memory at the given index
   def disallocate(self, pointer):
     pass
-  
+
   # Return the current total (allocatable) free space
   def total_free_space(self):
     pass
-  
+
   # Return the current total allocated memory
   def total_allocated_space(self):
     pass
