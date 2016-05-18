9e220e6ebcad1b66a0b35e26d45d65b5b679caa8
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-05-18 12:17:34 +0000

    Lab 7 - Added Lab 7.

--- Lab6/Tests/header_helpers.py
+++ Lab6/Tests/header_helpers.py
@@ -6,19 +6,19 @@ import unittest
 class HeadersHelpers(unittest.TestCase):
   def test_header_get_garbage_flag(self):
     b = b'\x10\x15\x12\x81\x51\x15\x12\x12\x45\x12\x52\x23\x23\x00\xd0\xff'
-    
+    print(b.hex())
     self.assertTrue(GC.header_get_garbage_flag(b, 0))
     self.assertFalse(GC.header_get_garbage_flag(b, 4))
     self.assertFalse(GC.header_get_garbage_flag(b, 8))
     self.assertTrue(GC.header_get_garbage_flag(b, 12))
+
   def test_header_set_garbage_flag(self):
     b = bytearray(b'\x10\x15\x12\x81\x51\x15\x12\x12\x45\x12\x52\x23\x23\x00\xd0\xff')
-    
     GC.header_set_garbage_flag(b, 0, False)
     GC.header_set_garbage_flag(b, 4, True)
     GC.header_set_garbage_flag(b, 8, True)
     GC.header_set_garbage_flag(b,12, False)
-    
+
     self.assertFalse(GC.header_get_garbage_flag(b, 0))
     self.assertTrue(GC.header_get_garbage_flag(b, 4))
     self.assertTrue(GC.header_get_garbage_flag(b, 8))
@@ -27,7 +27,7 @@ class HeadersHelpers(unittest.TestCase):
 
   def test_header_get_used_flag(self):
     b = b'\x10\x15\x12\x81\x51\x15\x12\x12\x45\x12\x52\x23\x23\x00\xd0\xff'
-    
+
     self.assertFalse(GC.header_get_used_flag(b, 0))
     self.assertFalse(GC.header_get_used_flag(b, 4))
     self.assertFalse(GC.header_get_used_flag(b, 8))
@@ -35,12 +35,12 @@ class HeadersHelpers(unittest.TestCase):
 
   def test_header_set_used_flag(self):
     b = bytearray(b'\x10\x15\x12\x81\x51\x15\x12\x12\x45\x12\x52\x23\x23\x00\xd0\xff')
-    
+
     GC.header_set_used_flag(b, 0, False)
     GC.header_set_used_flag(b, 4, True)
     GC.header_set_used_flag(b, 8, True)
     GC.header_set_used_flag(b,12, False)
-    
+
     self.assertFalse(GC.header_get_used_flag(b, 0))
     self.assertTrue(GC.header_get_used_flag(b, 4))
     self.assertTrue(GC.header_get_used_flag(b, 8))
@@ -49,29 +49,29 @@ class HeadersHelpers(unittest.TestCase):
 
   def test_header_is_pointers_array(self):
     b = b'\x10\x15\x12\x81\x51\x15\x12\x12\x45\x12\x52\x23\x23\x00\xd0\xff'
-    
+
     self.assertFalse(GC.header_is_pointers_array(b, 0))
     self.assertFalse(GC.header_is_pointers_array(b, 4))
     self.assertTrue(GC.header_is_pointers_array(b, 8))
     self.assertTrue(GC.header_is_pointers_array(b, 12))
-    
+
   def test_header_mark(self):
     b = bytearray(b'\x10\x15\x12\x81\x51\x15\x12\x12\x45\x12\x52\x23\x23\x00\xd0\xff')
-    
+
     GC.header_mark_as_pointers_array(b, 0)
     GC.header_mark_as_bytes_array(b, 4)
     GC.header_mark_as_pointers_array(b, 8)
     GC.header_mark_as_bytes_array(b,12)
-    
+
     self.assertTrue(GC.header_is_pointers_array(b, 0))
     self.assertFalse(GC.header_is_pointers_array(b, 4))
     self.assertTrue(GC.header_is_pointers_array(b, 8))
     self.assertFalse(GC.header_is_pointers_array(b, 12))
     self.assertEqual(b, bytearray(b'\x10\x15\x12\xa1Q\x15\x12\x12E\x12R##\x00\xd0\xdf'))
-  
+
   def test_header_get_size(self):
     b = bytearray(b'\x10\x15\x12\x81\x51\x15\x12\x12\x45\x12\x52\x23\x23\x00\xd0\xff')
-    self.assertEqual(GC.header_get_size(b,0), 17962256 )
+    self.assertEqual(GC.header_get_size(b,0), 17962256)
     self.assertEqual(GC.header_get_size(b,4), 303174993 )
     self.assertEqual(GC.header_get_size(b,8), 55710277)
     self.assertEqual(GC.header_get_size(b,12),533725219 )
@@ -86,6 +86,6 @@ class HeadersHelpers(unittest.TestCase):
     #self.assertEqual(GC.header_get_size(b,4), 241)
     #self.assertEqual(GC.header_get_size(b,8), 5334)
     #self.assertEqual(GC.header_get_size(b,12), 1242412)
-    
+
 if __name__ == '__main__':
   unittest.main()
