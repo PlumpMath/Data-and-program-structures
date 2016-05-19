2aae501bce7c015b8d808fbccd811c7b3e457c11
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-05-19 12:54:47 +0000

    Lab 6 - Now passing header_get_size tests.

--- Lab6/src/GC/header.py
+++ Lab6/src/GC/header.py
@@ -22,11 +22,11 @@ def header_mark_as_bytes_array(heap, pointer):
   pass
 
 def header_get_size(heap, pointer):
-  number = bytearray()
+  ba = bytearray()
   for i in range (0, 4):
-    number.append(heap[pointer+i])
-  print("Number:", number)
-  return int.from_bytes(number, 'little')
+    ba.append(heap[pointer+i])
+  number = int.from_bytes(ba, 'little')
+  return number & 0b00011111111111111111111111111111
 
 def header_set_size(heap, pointer, size):
   pass
